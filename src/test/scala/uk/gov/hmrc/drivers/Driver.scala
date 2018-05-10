dead3eb4982e9c6110e0d5e6cd6769e879ee03d6
rashmithreddy <rashmithreddy1987@gmail.com>
2018-05-10 16:48:25 +0100

    updated build file to generate HTML reports

--- src/test/scala/uk/gov/hmrc/drivers/Driver.scala
+++ src/test/scala/uk/gov/hmrc/drivers/Driver.scala
@@ -5,14 +5,14 @@ import org.openqa.selenium.chrome.{ChromeDriver, ChromeDriverService}
 
 object Driver {
 
-  def getOs = System.getProperty("os.name")
+  def getOs: String = System.getProperty("os.name")
 
-  lazy val isMac = getOs.startsWith("Mac")
+  lazy val isMac: Boolean = getOs.startsWith("Mac")
 
-  if(isMac) {
+  if (isMac) {
     System.setProperty(ChromeDriverService.CHROME_DRIVER_EXE_PROPERTY, "./src/test/drivers/chrome/chromedriverMac")
     System.setProperty("webdriver.gecko.driver", System.getProperty("user.dir") + "/src/test/resources/geckodriver")
-  }else {
+  } else {
 
   }
 
