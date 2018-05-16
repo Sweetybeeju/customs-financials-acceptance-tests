b527f8a2f867c9561764e38703f603c8f4e9b0aa
rashmithreddy <rashmithreddy1987@gmail.com>
2018-05-16 17:24:41 +0100

    explicit waits added

--- src/test/scala/uk/gov/hmrc/pages/WebPage.scala
+++ src/test/scala/uk/gov/hmrc/pages/WebPage.scala
@@ -1,19 +1,28 @@
 package uk.gov.hmrc.pages
 
-import org.openqa.selenium.{By, WebElement}
+import java.util.concurrent.TimeUnit
+
+import org.openqa.selenium.support.ui.{ExpectedConditions, FluentWait}
+import org.openqa.selenium.{By, StaleElementReferenceException, WebDriver, WebElement}
 import org.scalatest.Matchers
 import org.scalatest.concurrent.Eventually
 import org.scalatest.selenium.{Page, WebBrowser}
-import uk.gov.hmrc.drivers.Driver
+import uk.gov.hmrc.drivers.Env
 
 trait WebPage extends Page with WebBrowser with Matchers with Eventually {
-  implicit val webdriver = Driver.driver
+  implicit val webdriver = Env.driver
 
-  def xpathElement(key : String) = {
-   find(xpath(key)).get.underlying
+  var fluentWait: FluentWait[WebDriver] = new FluentWait[WebDriver](webdriver)
+    .withTimeout(Env.waitTime, TimeUnit.SECONDS)
+    .pollingEvery(1, TimeUnit.SECONDS)
+    .ignoring(classOf[NoSuchElementException])
+    .ignoring(classOf[StaleElementReferenceException])
+
+  def assertPageTitle(title: String) = {
+    pageTitle shouldBe title
   }
 
-  def assertPageTitle(title:String) = {
-    pageTitle shouldBe title
+  def waitForElementVisibility(webelement: WebElement): WebElement = {
+    fluentWait.until(ExpectedConditions.visibilityOf(webelement))
   }
 }
