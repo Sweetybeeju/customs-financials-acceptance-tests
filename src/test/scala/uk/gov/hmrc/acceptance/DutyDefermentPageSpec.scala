57d752050e2250d5114c4bba48c3a0a4fb85c6a5
Christopher Townson <iteratoruk@gmail.com>
2018-05-25 10:57:58 +0100

    MMCA-517 obtain expectations dynamically by comparison of page display and actual download

--- src/test/scala/uk/gov/hmrc/acceptance/DutyDefermentPageSpec.scala
+++ src/test/scala/uk/gov/hmrc/acceptance/DutyDefermentPageSpec.scala
@@ -1,6 +1,6 @@
 package uk.gov.hmrc.acceptance
 
-import uk.gov.hmrc.pages.DutyDefermentListPage
+import uk.gov.hmrc.pages.{DownloadedFile, DutyDefermentListPage}
 
 class DutyDefermentPageSpec extends AcceptanceSpec {
 
@@ -10,35 +10,35 @@ class DutyDefermentPageSpec extends AcceptanceSpec {
 
     val dutyDefermentPage = new DutyDefermentListPage()
 
-    ignore("File can be downloaded") {
+    scenario("File can be downloaded") {
       Given("i am on the duty deferment page")
       dutyDefermentPage.navigateToPage
       When("i select a statement for a period")
-      val statement: (Array[Byte], String) = dutyDefermentPage.selectStatement(1)
+      val statement: DownloadedFile = dutyDefermentPage.selectStatement(1)
       Then("i am able to access the pdf file for that period")
-      (statement._1.length > 0) should be(true)
-      statement._2 should be("application/pdf")
+      (statement.data.length > 0) should be(true)
+      statement.mimeType should be("application/pdf")
     }
 
-    ignore("Link text should be file name") {
+    scenario("Link text should be file name") {
       Given("i am on the duty deferment page")
       dutyDefermentPage.navigateToPage
       When("i select a statement for a period")
-      val statement: (Array[Byte], String) = dutyDefermentPage.selectStatement(1)
-      (statement._1.length > 0) should be(true)
+      val statement: DownloadedFile = dutyDefermentPage.selectStatement(1)
+      (statement.data.length > 0) should be(true)
       dutyDefermentPage.listOfStatements.nonEmpty should be(true)
       Then("the link text should be the file name")
-      dutyDefermentPage.getFileName should be("dummy-duty-deferment-statement")
+      dutyDefermentPage.getFileName(1) should be(statement.name)
     }
 
-    ignore("Understand the size of each PDF") {
+    scenario("Understand the size of each PDF") {
       Given("i am on the duty deferment page")
       dutyDefermentPage.navigateToPage
       When("i select a statement for a period")
-      val statement: (Array[Byte], String) = dutyDefermentPage.selectStatement(1)
-      (statement._1.length > 0) should be(true)
+      val statement: DownloadedFile = dutyDefermentPage.selectStatement(1)
+      (statement.data.length > 0) should be(true)
       Then("i am able to understand the size of each PDF")
-      dutyDefermentPage.sizeOfStatement(1) should be(EXPECTED_TEST_FILE_SIZE_1_1MB)
+      dutyDefermentPage.sizeOfStatement(1) should be(statement.sizeDescription)
     }
   }
 }
--- src/test/scala/uk/gov/hmrc/pages/DutyDefermentListPage.scala
+++ src/test/scala/uk/gov/hmrc/pages/DutyDefermentListPage.scala
@@ -3,6 +3,8 @@ package uk.gov.hmrc.pages
 import com.typesafe.scalalogging.LazyLogging
 import org.openqa.selenium.{By, WebDriver}
 import org.scalatest.concurrent.ScalaFutures
+import play.api.libs.ws.StandaloneWSResponse
+import play.libs.ws.StandaloneWSRequest
 
 import scala.collection.JavaConversions
 import scala.concurrent.Await
@@ -13,13 +15,13 @@ class DutyDefermentListPage(implicit webdriver: WebDriver) extends WebPage with 
 
   override val url: String = s"${config.app.endpoint}/customs-financials/duty-deferment"
 
-  def selectStatement(i: Int): (Array[Byte], String) = {
+  def selectStatement(i: Int): DownloadedFile = {
     captureLinkContent(find(cssSelector(s".duty-deferment-statements li:nth-child(${i}) a")).get.underlying.getAttribute("href"))
   }
 
-  def getFileName: String = {
-    waitForElement(By.linkText("dummy-duty-deferment-statement"))
-    find(linkText("dummy-duty-deferment-statement")).get.text
+  def getFileName(i: Int): String = {
+    waitForElement(By.cssSelector(s".duty-deferment-statements li:nth-child(${i}) a"))
+    find(cssSelector(s".duty-deferment-statements li:nth-child(${i}) a")).get.text
   }
 
   def sizeOfStatement(i: Int): String = {
@@ -27,10 +29,10 @@ class DutyDefermentListPage(implicit webdriver: WebDriver) extends WebPage with 
     sizeInWords
   }
 
-  private def captureLinkContent(url: String): (Array[Byte], String) = {
+  private def captureLinkContent(url: String): DownloadedFile = {
     Await.result(
       ws.url(url).get().map { r =>
-        (r.bodyAsBytes.toArray, r.contentType)
+        DownloadedFile(r)
       }, 10.seconds)
   }
 
@@ -41,5 +43,26 @@ class DutyDefermentListPage(implicit webdriver: WebDriver) extends WebPage with 
 
 }
 
+case class DownloadedFile(data: Array[Byte], mimeType: String, disposition: String, name: String) {
 
+  def sizeDescription: String = data.length match {
+    case kb if 1000 until 1000000 contains kb => f"${kb.toFloat / 1000}%1.1f kB"
+    case mb if mb >= 1000000 => f"${mb.toFloat / 1000000}%1.1f MB"
+    case _ => data.length + " bytes"
+  }
+
+}
+
+object DownloadedFile {
+
+  def apply(resp: StandaloneWSResponse): DownloadedFile = {
+    val data = resp.bodyAsBytes.toArray
+    val mimeType = resp.contentType
+    val hdr = resp.header("Content-Disposition").getOrElse("").split(";").take(2).map(_.trim).toList
+    val disposition = hdr(0)
+    val name: String = hdr(1).split("=").take(2).map(_.trim).toList(1).replaceAll("\"", "")
+    DownloadedFile(data, mimeType, disposition, name)
+  }
+
+}
 
