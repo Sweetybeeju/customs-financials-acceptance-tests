07d635b5e1089e0c39eea59d9207a95d03effb22
david.ellam <ellamdav@users.noreply.github.com>
2018-06-21 15:39:22 +0100

    MMCA-737 WIP DD statement view - period and liability sections

--- src/test/resources/features/DutyDefermentStatementView.feature
+++ src/test/resources/features/DutyDefermentStatementView.feature
@@ -3,19 +3,27 @@ Feature: Duty Deferment Statement View Page
 
   Background:
     Given the Duty Deferment feature is enabled
+#    TODO Given I am signed in as a registered user
     And I have at least one Statement available to view
 
   Scenario: The total liabilities the month in which the statement was issued
+#    TODO separate scenario for not-signed-in redirect
     Given I am not signed in
     When I navigate to the Duty Deferment Statement View page
     And I am redirected to the Sign In page
     And I sign in as a registered user
+    And I navigate to the Duty Deferment Statement View page
+#    Then the page title should be X
     Then I am able to see the period in which the Statement was issued
     And I am able to see the total liabilities for the period in which the Statement was issued
+      | Duty Due | VAT Due  | Excise Due |
+      | £2000.00 | £3000.00 | £1000.00   |
     And I am able to see the total liabilities for the month in which the Statement was issued
-
-  Scenario:The list of transactions, ordered by accounting date ASCENDING
-    When I navigate to the Duty Deferment Statement View page
+      | Duty Due | VAT Due  | Excise Due |
+      | £6000.00 | £9001.00 | £3333.00   |
+#
+#  Scenario:The list of transactions, ordered by accounting date ASCENDING
+#    When I navigate to the Duty Deferment Statement View page
     Then I can see the following transactions
       | Accounting Date | Movement Reference Number | Agent's EORI   | Agent's Reference | Duty Due    | VAT Due     | Excise Due  |
       | 2018-02-20	    | 	    12345678ABC         |  EORI123456    |     AGENT98765    | 	 £1234.56  | 	£123.45  |  £12.34     |
--- src/test/scala/uk/gov/hmrc/stepdefs/DutyDefermentStatementViewSteps.scala
+++ src/test/scala/uk/gov/hmrc/stepdefs/DutyDefermentStatementViewSteps.scala
@@ -3,7 +3,7 @@ package uk.gov.hmrc.stepdefs
 import java.util.NoSuchElementException
 import scala.collection.JavaConverters._
 
-import cucumber.api.{DataTable, PendingException}
+import cucumber.api.DataTable
 import cucumber.api.scala.{EN, ScalaDsl}
 import org.jsoup.Jsoup
 import org.jsoup.nodes.Document
@@ -20,21 +20,23 @@ class DutyDefermentStatementViewSteps extends WebPage with ScalaDsl with EN with
 
   Then("""^I am able to see the period in which the Statement was issued$"""){ () =>
     // TODO include period year/month/number explicitly in scenario
-    pageElement(".period .year").toInt should be (2018)
-    pageElement(".period .month").toInt should be (2)
-    pageElement(".period .number").toInt should be (3)
+    pageElement(".period") should be ("Period 2018/02 - Statement Number 3")
   }
 
-  Then("""^I am able to see the total liabilities for the (period|month) in which the Statement was issued$"""){ periodType: String =>
-    // TODO explicitly state amounts in scenario
-    pageElement(s".total-liabilities .$periodType .vat-due").toFloat should be (9001.00)
-    pageElement(s".total-liabilities .$periodType .duty-due").toFloat should be (6000.00)
-    pageElement(s".total-liabilities .$periodType .excise-due").toFloat should be (3333.00)
+  Then("""^I am able to see the total liabilities for the (period|month) in which the Statement was issued$"""){ (periodType: String, liabilities: DataTable) =>
+    val liabilitiesMap = liabilities.asMaps(classOf[String],classOf[String]).asScala.head.asScala
+    for ((k, v) <- liabilitiesMap) {
+      val cssClass = k.toLowerCase().replace(" ", "-")
+      pageElement(s".total-liabilities .for-$periodType .$cssClass") should be (v)
+    }
+//
+//    pageElement(s".total-liabilities .for-$periodType .vat-due").toFloat should be (9001.00)
+//    pageElement(s".total-liabilities .for-$periodType .duty-due").toFloat should be (6000.00)
+//    pageElement(s".total-liabilities .for-$periodType .excise-due").toFloat should be (3333.00)
   }
 
   Then("""^I can see the following transactions$"""){ transactions: DataTable =>
-    // TODO match scenario transactions to those in the webpage
-
+    // TODO extract helper method
     // TODO may be a cleaner way to do this...? maybe just jsoup.parse the whole page in the page object?
     val transactionsHTML = find(cssSelector(".transactions")).get.underlying.getAttribute("outerHTML")
     val doc: Document = Jsoup.parseBodyFragment(transactionsHTML)
@@ -61,10 +63,9 @@ class DutyDefermentStatementViewSteps extends WebPage with ScalaDsl with EN with
       }
     }
 
-
   }
 
-
+  // TODO extract to WebPage and rename
   private def pageElement(selector: String) = {
     try {
       find(cssSelector(selector)).get.text.trim
