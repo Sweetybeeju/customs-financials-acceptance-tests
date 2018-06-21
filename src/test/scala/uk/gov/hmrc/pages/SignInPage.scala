c63a50b6bb2ee67a850a05d964e15c9fd3e973b2
Christopher Townson <iteratoruk@gmail.com>
2018-06-21 16:27:52 +0100

    MMCA-735 verify DD statement display meets requirements

--- src/test/scala/uk/gov/hmrc/pages/SignInPage.scala
+++ src/test/scala/uk/gov/hmrc/pages/SignInPage.scala
@@ -11,10 +11,15 @@ trait SignInPage extends WebPage {
 
   override val url: String = Configuration.settings.SIGN_IN_PAGE
 
-  def continueUrl: Option[String] = new URL(webDriver.getCurrentUrl).getQuery.split("&").map(v => {
-    val m =  v.split("=", 2).map(s => URLDecoder.decode(s, StandardCharsets.UTF_8.name()))
-    m(0) -> m(1)
-  }).toMap.get("continue")
+  def continueUrl: Option[String] = {
+    val q = Option(new URL(webDriver.getCurrentUrl).getQuery)
+    q.flatMap { str =>
+      str.split("&").map(v => {
+        val m =  v.split("=", 2).map(s => URLDecoder.decode(s, StandardCharsets.UTF_8.name()))
+        m(0) -> m(1)
+      }).toMap.get("continue")
+    }
+  }
 
   override def isCurrentPage: Boolean = getTitle == "Sign in - Government Gateway"
 }
